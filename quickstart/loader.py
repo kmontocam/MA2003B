fcbd5240eaf3c918b1ee2e8e5ff1f13f491c0e1f
Kevin Montoya <A01740352@tec.mx>
2022-08-20 14:22:22 -0500

    Release: module to connect with folder and access databases

---
+++ quickstart/loader.py
@@ -0,0 +1,61 @@
+import pydrive
+import pickle
+import pandas as pd
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from typing import Callable
+# add annotations for __new__
+
+class XlsxDriveLoader():
+    """
+    Read Excel files in Drive Folder
+    """
+
+    _loaded = None
+    _folder_id = '1XoUes99yRgfm8KNT2Kf9ZXQBexos_2dm'
+
+    def __new__(cls) -> dict:
+        if ((databases := cls._loaded) is not None):
+            print(
+                'Database has already been loaded in memory and as a .pkl file in .data/')
+            return databases
+
+        databases = super().__new__(cls)
+        cls._loaded = databases
+        GoogleAuth.DEFAULT_SETTINGS['client_config_file'] = './quickstart/client_secrets.json'
+        gauth = GoogleAuth()
+        gauth.LocalWebserverAuth()
+        drive = GoogleDrive(gauth)
+        databases._load_databases(drive, cls._folder_id)
+
+        return databases
+
+    def _load_databases(self, drive, folder_id) -> None:
+
+        data_container = drive.ListFile(
+            {'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
+
+        load_databases: Callable[[pydrive.files.GoogleDriveFile],
+                                 None] = lambda x: x.FetchContent()
+        gen = (map(load_databases, data_container))
+
+        while True:
+            try:
+                next(gen)
+            except StopIteration:
+                break
+
+        databases = dict()
+        for xlsx in data_container:
+            if xlsx['title'].endswith('.xlsx'):
+                xlsx_content = pd.ExcelFile(xlsx.content)
+
+                xlsx_sheets = dict()
+                for sheet_name in xlsx_content.sheet_names:
+                    xlsx_sheets[sheet_name] = xlsx_content.parse(sheet_name)
+
+                databases[xlsx['title']] = xlsx_sheets
+
+        self.content = databases
+        with open('data/databases.pkl', 'wb') as file:
+            pickle.dump(databases, file)
