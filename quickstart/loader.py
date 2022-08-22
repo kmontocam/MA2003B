1511e16d9cd180e94c2fb5d3ec949767503f83f6
Kevin Montoya <A01740352@tec.mx>
2022-08-22 12:37:48 -0500

    Hotfix: changed loaded status

--- quickstart/loader.py
+++ quickstart/loader.py
@@ -3,44 +3,45 @@ import pickle
 import pandas as pd
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from typing import Callable
+from typing import Callable, Type
 
 
 class XlsxDriveLoader():
     """
-    Read Excel files in Drive Folder
+    Read Excel files in Drive Folder, organize them and load them locally without explictily
+    downloading .xlsx files
     """
     _loaded = None
     _folder_id = '1XoUes99yRgfm8KNT2Kf9ZXQBexos_2dm'
 
-    def __new__(cls) -> dict:
+    def __new__(cls: Type) -> dict:
         if ((databases := cls._loaded) is not None):
-            print(
-                'Database has already been loaded in memory and as a .pkl file in .data/')
+            print('Database has already been loaded in memory and as a .pkl file in .data/')
             return databases
 
         databases = super().__new__(cls)
-        cls._loaded = databases
         GoogleAuth.DEFAULT_SETTINGS['client_config_file'] = './quickstart/client_secrets.json'
         gauth = GoogleAuth()
         gauth.LocalWebserverAuth()
         drive = GoogleDrive(gauth)
         databases._load_databases(drive, cls._folder_id)
+        
+        cls._loaded = databases
 
         return databases
 
     def _load_databases(self, drive, folder_id) -> None:
 
-        data_container = drive.ListFile(
-            {'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
+        data_container = drive.ListFile({'q': f"'{folder_id}' in parents and trashed=false"}
+                                        ).GetList()
         load_databases: Callable[[pydrive.files.GoogleDriveFile], None] = (
-            lambda x: x.FetchContent())
-        gen = (map(load_databases, data_container))
+                                  lambda file: file.FetchContent())
+        mapper = (map(load_databases, data_container))
 
         # Allocate data in memory
         while True:
             try:
-                next(gen)
+                next(mapper)
             except StopIteration:
                 break
 
@@ -60,3 +61,5 @@ class XlsxDriveLoader():
         self.content = databases
         with open('data/raw_databases.pkl', 'wb') as file:
             pickle.dump(databases, file)
+        
+        print('Data was correctly allocated in memory')
